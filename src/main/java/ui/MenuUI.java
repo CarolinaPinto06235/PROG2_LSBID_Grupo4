aa8b63dc0d0d98a228a4012b44d7fc251cde4948
CarolinaPinto0623 <up202406235@up.pt>
2025-05-16 18:38:24 +0100

    Alterações 16.05.25 18:38

--- src/main/java/ui/MenuUI.java
+++ src/main/java/ui/MenuUI.java
@@ -33,15 +33,14 @@ public class MenuUI {
             System.out.println("----------------- |  1 - Introdução de dados.                                 |  -----------------");
             System.out.println("----------------- |  2 - Leitura de dados a partir de ficheiros.              |  -----------------");
             System.out.println("----------------- |  3 - Visualização de todos os dados.                      |  -----------------");
-            System.out.println("----------------  |  4 - Escolher um técnico de Saúde.                        |  -----------------");
-            System.out.println("----------------  |  5 - Ordenar pacientes por data de nascimento.            |  -----------------");
-            System.out.println("----------------  |  6 - Ordenar técnicos de saúde por nome.                  |  -----------------");
-            System.out.println("----------------- |  7 - Alteração dos sinais vitais.                         |  -----------------");
-            System.out.println("----------------- |  8 - Percentagem de pacientes em situação crítica.        |  -----------------");
-            System.out.println("----------------- |  9 - Estatísticas dos pacientes.                          |  -----------------");
-            System.out.println("----------------- | 10 - Determinação do score de gravidade de um paciente.   |  -----------------");
-            System.out.println("----------------- | 11 - Guardar e visualizar dados do ficheiro.              |  -----------------");
-            System.out.println("----------------- | 12 - Visualização de gráficos de barras.                  |  -----------------");
+            System.out.println("----------------  |  4 - Ordenar pacientes por data de nascimento.            |  -----------------");
+            System.out.println("----------------  |  5 - Ordenar técnicos de saúde por nome.                  |  -----------------");
+            System.out.println("----------------- |  6 - Alteração dos sinais vitais.                         |  -----------------");
+            System.out.println("----------------- |  7 - Percentagem de pacientes em situação crítica.        |  -----------------");
+            System.out.println("----------------- |  8 - Estatísticas dos pacientes.                          |  -----------------");
+            System.out.println("----------------- |  9 - Determinação do score de gravidade de um paciente.   |  -----------------");
+            System.out.println("----------------- | 10 - Guardar e visualizar dados do ficheiro.              |  -----------------");
+            System.out.println("----------------- | 11 - Visualização de gráficos de barras.                  |  -----------------");
             System.out.println("----------------- |  0 - Terminar aplicação.                                  |  -----------------");
             System.out.println("--------------------------------------------------------------------------------------------------");
 
@@ -58,24 +57,22 @@ public class MenuUI {
                 System.out.println("Selecionou a opção: Visualização de dados no ecrã.");
                 Hospital.visualizarDados();
             } else if (opcao.equals("4")) {
-                System.out.println("Selecionou a opção: Escolher um técnico de saúde.");
-            } else if (opcao.equals("5")) {
                 System.out.println("Selecionou a opção: Ordenar pacientes por data de nascimento.");
-            } else if (opcao.equals("6")) {
+            } else if (opcao.equals("5")) {
                 System.out.println("Selecionou a opção: Ordenar técnicos de saúde por nome.");
-            } else if (opcao.equals("7")) {
+            } else if (opcao.equals("6")) {
                 System.out.println("Selecionou a opção: Alteração dos sinais vitais.");
-            } else if (opcao.equals("8")) {
+            } else if (opcao.equals("7")) {
                 System.out.println("Selecionou a opção: Percentagem de pacientes em situação crítica.");
-            } else if (opcao.equals("9")) {
+            } else if (opcao.equals("8")) {
                 Estatisticas estatisticas = new Estatisticas();
                 estatisticas.calcularEstatisticas(hospital);
                 System.out.println("Selecionou a opção: Estatísticas dos pacientes (um paciente, um grupo de pacientes ou de todos os pacientes).");
-            } else if (opcao.equals("10")) {
+            } else if (opcao.equals("9")) {
                 System.out.println("Selecionou a opção: Determinação do score de gravidade de um paciente.");
-            } else if (opcao.equals("11")) {
+            } else if (opcao.equals("10")) {
                 System.out.println("Selecionou a opção: Guardar e visualizar dados do ficheiro.");
-            } else if (opcao.equals("12")) {
+            } else if (opcao.equals("11")) {
                 System.out.println("Selecionou a opção: Visualização de gráficos de barras.");
             }
             while (!opcao.equals("0")) ;
